89ebe33d99af32f28c81e39a408cae8662f47674
KawaiKishiTR <aselafen@gmail.com>
2025-09-28 01:12:49 +0300

    printf function implementation

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -2,5 +2,9 @@
 #include "stdio.h"
 
 void _cdecl cstart_(){
-    puts("Hello World from C!");
+    puts("Kernel Succesfully Loaded\x0D\x0A");
+    puts("Hello World from C!\x0D\x0A");
+
+    printf("formatted: %% %c %s\r\n", 'f', "hello");
+    printf("%d,%i, %x, %p, %ld", 1234, -2134, 0x1a, 0x3a, -1000000000l);
 }
