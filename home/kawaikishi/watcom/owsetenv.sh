fcfd5ead7e2b8363add2b799f833f647bc386db1
KawaiKishiTR <aselafen@gmail.com>
2025-09-27 19:22:18 +0300

    accidentally added linker fix

--- home/kawaikishi/watcom/owsetenv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo Open Watcom Build Environment
-export PATH=home/kawaikishi/watcom/binl64:home/kawaikishi/watcom/binl:$PATH
-export INCLUDE=home/kawaikishi/watcom/lh:$INCLUDE
-export WATCOM=home/kawaikishi/watcom
-export EDPATH=home/kawaikishi/watcom/eddat
-export WWINHELP=home/kawaikishi/watcom/binw
-export WIPFC=home/kawaikishi/watcom/wipfc
