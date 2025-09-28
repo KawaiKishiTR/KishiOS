6e4e78ccfa5e1729ced5a53323c1c866609aa6c1
KawaiKishiTR <aselafen@gmail.com>
2025-09-28 15:01:27 +0300

    disk reset function added

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,10 +1,12 @@
 #include "stdint.h"
-#include "stdio.h"
+#include "stdio/stdio.h"
+#include "disk/asmDisk.h"
 
 void _cdecl cstart_(){
+    uint8_t error;
+
+    x86_Disk_Reset(0, &error);
+    printf("Error %d\r\n", error);
     puts("Kernel Succesfully Loaded\x0D\x0A");
     puts("Hello World from C!\x0D\x0A");
-
-    printf("formatted: %% %c %s\r\n", 'f', "hello");
-    printf("%d,%i, %x, %p, %ld", 1234, -2134, 0x1a, 0x3a, -1000000000l);
 }
